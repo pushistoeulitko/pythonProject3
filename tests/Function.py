f2435f8e6e7fc3ea5b122821df2bc97c972279f7
pushistoeulitko <68023783+pushistoeulitko@users.noreply.github.com>
2021-05-02 23:05:31 +0500

    +все функции для функции -осталось функцию-обертку доредактировать

--- tests/Function.py
+++ tests/Function.py
@@ -3,8 +3,8 @@ from tests import DATA
 from collections import Counter
 
 collect_date = []
-sort_collect = []
 collect_with_add_element = []
+sort_collect = []
 
 
 def print_value(func):
@@ -30,45 +30,45 @@ def loading():
         return collect_date
 
 
-#@print_value(sort_collect)
-def sorting(sort=bool):
-    if sort == bool:
-        sort_type = False
-    if sort != bool:
-        sort_type = True
-    sort_items = sorted(collect_date, key=lambda data: int(data.simple_value), reverse=sort_type)
-    sort_collect.extend(sort_items)
-    return sort_collect
-
-
 #@print_value(collect_with_add_element)
 def adding_an_additional_parameter():
     num_list = []
-    for i in range(len(sort_collect) - 1):
-        num = sort_collect[i].simple_value
+    for i in range(len(collect_date) - 1):
+        num = collect_date[i].simple_value
         num_list.append(num)
     a = Counter(num_list)
-    for y in range(len(sort_collect) - 1):
-        b = sort_collect[y].simple_value
-        c = sort_collect[y].simple_key
+    for y in range(len(collect_date) - 1):
+        b = collect_date[y].simple_value
+        c = collect_date[y].simple_key
         simple_count = a.get(b)
         obj2 = DATA.Data(c, b, simple_count)
         collect_with_add_element.append(obj2)
     return collect_with_add_element
 
 
+#@print_value(sort_collect)
+def sorting(sort=bool):
+    if sort == bool:
+        sort_type = True
+    if sort != bool:
+        sort_type = False
+    sort_items = sorted(collect_with_add_element, key=lambda data: int(data.simple_count), reverse=sort_type)
+    sort_collect.extend(sort_items)
+    return sort_collect
+
+
 #@print_value(collect_with_add_element)
 def saving_to_a_file():
     with open("simple2.dat", "w", encoding="utf-8") as file:
-        for y in range(len(collect_with_add_element) - 1):
-            line = str(collect_with_add_element[y]) + '\n'
+        for y in range(len(sort_collect) - 1):
+            line = str(sort_collect[y]) + '\n'
             file.write(line)
 
 
 def load_sort_add_counter_and_save_in_file():
     loading()
-    sorting()
     adding_an_additional_parameter()
+    sorting()
     saving_to_a_file()
 
 
