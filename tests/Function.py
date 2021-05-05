1b12ef6e7ada80a75a47482363fb93b818969845
pushistoeulitko <68023783+pushistoeulitko@users.noreply.github.com>
2021-05-05 23:14:43 +0500

    +убрала глобальную переменную +в явном виде вынесла, все, что можно в аргументы конечной функции +добавила в гит игнор все и сам гит-игнор тоже

--- tests/Function.py
+++ tests/Function.py
@@ -2,63 +2,68 @@ import re
 from tests import DATA
 from collections import Counter
 
-collect_date = []
-
 
 def print_value(func):
-    def wrapper():
-        func()
-        last_ten = ''.join(str(collect_date[len(collect_date) - 10:]))
-        first_ten = ''.join(str(collect_date[:10]))
+    def wrapper(*args):
+        func(*args)
+        last_ten = ''.join(str(args[0][len(args[0]) - 10:]))
+        first_ten = ''.join(str(args[0][:10]))
         print('Первые 10 значений:\n' + first_ten + '\nПоследние 10 значений:\n' + last_ten)
+        return args
+
     return wrapper
 
 
 @print_value
-def loading():
-    with open("simple.dat", "r") as file:
+def loading(collect, sort_file):
+    with open(sort_file, "r") as file:
         for line in file:
             simple_key = re.findall(r'(\b\w+\b)', line)[0]
             simple_value = re.findall(r'\b\d+\b', line)[0]
             obj = DATA.Data(simple_key, simple_value, '')
-            collect_date.append(obj)
-        return collect_date
+            collect.append(obj)
+        return collect
 
 
 @print_value
-def adding_an_additional_parameter():
+def adding_an_additional_parameter(collect):
     num_list = []
-    for i in range(len(collect_date)):
-        num = collect_date[i].simple_value
+    for i in range(len(collect)):
+        num = collect[i].simple_value
         num_list.append(num)
     dict_frequency = Counter(num_list)
-    for y in range(len(collect_date)):
-        date_y = collect_date[y]
+    for y in range(len(collect)):
+        date_y = collect[y]
         val = date_y.simple_value
         simple_count = dict_frequency.get(val)
         date_y.simple_count = simple_count
+    return collect
 
 
 @print_value
-def sorting(sort_type=False):
-    if sort_type != bool:
-        sort_type = True
-    collect_date.sort(key=lambda data: int(data.simple_count), reverse=sort_type)
+def sorting(collect, sorting_order):
+    if sorting_order == 'desc':
+        sort = False
+    if sorting_order == 'asc':
+        sort = True
+    collect.sort(key=lambda data: int(data.simple_count), reverse=sort)
+    return collect
 
 
 @print_value
-def saving_to_a_file():
-    with open("simple2.dat", "w") as file:
-        for y in range(len(collect_date)):
-            line = str(collect_date[y]) + '\n'
+def saving_to_a_file(collect, output_file):
+    with open(output_file, "w") as file:
+        for y in range(len(collect)):
+            line = str(collect[y]) + '\n'
             file.write(line)
 
 
-def load_sort_add_counter_and_save_in_file():
-    loading()
-    adding_an_additional_parameter()
-    sorting()
-    saving_to_a_file()
+def load_sort_add_counter_and_save_in_file(sort_file, sorting_order, output_file):
+    collect = []
+    loading(collect, sort_file)
+    adding_an_additional_parameter(collect)
+    sorting(collect, sorting_order)
+    saving_to_a_file(collect, output_file)
 
 
-load_sort_add_counter_and_save_in_file()
+load_sort_add_counter_and_save_in_file("simple.dat", 'desc', "simple3.dat")
